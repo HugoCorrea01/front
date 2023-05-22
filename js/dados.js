84a14f693732045a7d6a58a0c4a0d0ae814af570
HUGO CORREA CALIXTO REGO FARRANHA <202301134391@alunos.ibmec.edu.br>
2023-05-22 11:38:52 -0300

    semana9

---
+++ js/dados.js
@@ -0,0 +1,17 @@
+const minhas_imagens = [
+    {
+        nome: 'Número_1',
+        valor: 1,
+        descricao: 'Número_1 com fundo amarelo.'
+    },
+    {
+        nome: 'Número_2',
+        valor: 2,
+        descricao: 'Número_2 com fundo vermelho.'
+    },
+    {
+        nome: 'Número_3',
+        valor: 3,
+        descricao: 'Número_3 vermelho em 3D.'
+    }
+];
